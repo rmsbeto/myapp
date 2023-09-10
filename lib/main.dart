039184c98a3a4c5bd84d78b5a03034939ef12a70
Roberto <robertomagrisantos@gmail.com>
2023-09-10 13:16:18 -0300

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // Algumas modificações realizadas por Robeto.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meu Contador',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Contador++'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'O Botão abaixo será utilizado para:',
+              style: TextStyle(color: Colors.green, fontSize: 25),
+            ),
+            const Text(
+              'Acrescentar mais um numero:',
+              style: TextStyle(color: Color.fromARGB(255, 16, 70, 18), fontSize: 20),
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add_box),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
